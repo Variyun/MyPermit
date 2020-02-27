5d50ae493322d14af7e80e33f741abf3824fc235
Aaron <aaron.chen1@ucalgary.ca>
2020-02-27 04:04:09 -0700

    Create new react project, add simple toolbar, Need to add drawer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar'
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
